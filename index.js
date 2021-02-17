99321c03d7cb26e9d8614caa1266f2eaf791f00b
Scott Bedard <scottbedard90@gmail.com>
2021-02-16 22:06:26 -0700

    create network to demonstrate coorelation of data size

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const { NeuralNetwork } = require('brain.js')
 
+// generate a 1 or 0
+const coinflip = () => Math.random() > 0.5 ? 1 : 0
+
+// generate an array of coinflips
+const seed = (length = 5) => new Array(length).fill().map(coinflip)
+
+
+// train a network to output the first value of an array
+// this output will become more accurate with more data
+// 10 -> 0.7598074078559875
+// 100 -> 0.9256471991539001
+// 1000 -> 0.9508631825447083
+// 10000 -> 0.9802496433258057
+// 100000 -> 0.9930176138877869
+// 1000000 -> 0.9974836707115173
 const network = new NeuralNetwork()
 
-network.train([
-  { input: [0, 1], output: [0] },
-  { input: [1, 0], output: [1] },
-])
+const data = new Array(1000)
+  .fill()
+  .map(() => {
+    const first = coinflip()
 
-const output = network.run([1, 0])
+    return {
+      input: [first, ...seed()],
+      output: [first]
+    }
+  })
+
+network.train(data)
+
+const output = network.run([1].concat(seed()))
 
 console.log(output)
