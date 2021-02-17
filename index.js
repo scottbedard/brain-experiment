4c69ed8de228a777d21e1b9a7671c07daa224897
Scott Bedard <scottbedard90@gmail.com>
2021-02-16 22:33:39 -0700

    load pre-trained model

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const { NeuralNetwork } = require('brain.js')
+const fs = require('fs')
+const path = require('path')
 
 // generate a 1 or 0
 const coinflip = () => Math.random() > 0.5 ? 1 : 0
@@ -6,30 +8,60 @@ const coinflip = () => Math.random() > 0.5 ? 1 : 0
 // generate an array of coinflips
 const seed = (length = 5) => new Array(length).fill().map(coinflip)
 
-
-// train a network to output the first value of an array
-// this output will become more accurate with more data
+// create a network to output the first value of an array
+// this should demonstrates the coorelation of data sizes
 // 10 -> 0.7598074078559875
 // 100 -> 0.9256471991539001
 // 1000 -> 0.9508631825447083
 // 10000 -> 0.9802496433258057
 // 100000 -> 0.9930176138877869
 // 1000000 -> 0.9974836707115173
+// 5000000 -> 0.9991652965545654
 const network = new NeuralNetwork()
 
-const data = new Array(1000)
-  .fill()
-  .map(() => {
-    const first = coinflip()
+const dataSize = 5000000
 
-    return {
-      input: [first, ...seed()],
-      output: [first]
-    }
-  })
+const train = () => {
+  console.log()
+  console.log('Network: NEW')
 
-network.train(data)
+  network.train(
+    new Array(dataSize).fill().map(() => {
+      const first = coinflip()
+  
+      return {
+        input: [first, ...seed()],
+        output: [first]
+      }
+    })
+  )
 
+  fs.writeFileSync(
+    path.resolve(__dirname, './model.json'),
+    JSON.stringify({ dataSize, network: network.toJSON() })
+  )
+}
+
+console.log()
+console.log(`Data size: ${dataSize}`)
+
+if (fs.existsSync(path.resolve(__dirname, './model.json'))) {
+  const model = require('./model.json')
+
+  if (model.dataSize === dataSize) {
+    console.log()
+    console.log('Network: CACHED')
+    network.fromJSON(model.network)
+  } else {
+    train();
+  }
+} else {
+  train()
+}
+
+// test the network
 const output = network.run([1].concat(seed()))
 
-console.log(output)
+console.log()
+console.log(`Output: ${output}`)
+console.log()
